aa157d972f494ef5df53e2e4e3b9ded1be02044f
Alex Cameron <72214882+alexcameron8@users.noreply.github.com>
2023-01-15 20:18:49 -0500

    GH pages deployment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 var particles = Particles.init({
     selector: '.background',
-    sizeVariations: 1,
-    color : ["#3b5998","#4267b2","#8b9dc3","#dfe3ee","#ffffff"],
+    sizeVariations: 2,
+    color : ["#a2aebb"],
     connectParticles : true,
 });
 
@@ -20,18 +20,21 @@ $(function() {
     var navbar = $(".navbar");
     var tabs = $(".tabs");
     var rightEffect = $(".rightEffect");
+    var downArrow = $(".down-arrow");
 
     $(window).scroll(function() {    
         var scroll = $(window).scrollTop();
         
-        if (scroll >= 400) {
+        if (scroll >= 350) {
             navbar.removeClass('navbar').addClass('navbar-alt');
             tabs.removeClass('tab').addClass('tab-alt');
             rightEffect.removeClass('rightEffect').addClass('pullUp');
+            downArrow.removeClass('.down-arrow').addClass('invisible');
         } else {
             navbar.removeClass("navbar-alt",'pullUp').addClass('navbar','rightEffect');
             tabs.removeClass('tab-alt').addClass('tab');
             rightEffect.removeClass('pullUp').addClass('rightEffect');
+            downArrow.removeClass('.down-arrow').addClass('invisible');
         }
     });
 });
@@ -41,25 +44,25 @@ $(window).on("load", function(){
 })
 
 window.addEventListener('scroll', function() {
-    var section1 = document.getElementById('section1');
-    var section2 = document.getElementById('section2');
-    var section3 = document.getElementById('section3');
-    var section4 = document.getElementById('section4');
-    var section5 = document.getElementById('section5');
+    // var section1 = document.getElementById('section1');
+    // var section2 = document.getElementById('section2');
+    // var section3 = document.getElementById('section3');
+    // var section4 = document.getElementById('section4');
+    // var section5 = document.getElementById('section5');
 
-    if (section1.getBoundingClientRect().top <= window.innerHeight) {
-        section1.classList.add('visible');
-    }
-    if (section2.getBoundingClientRect().top <= window.innerHeight) {
-        section2.classList.add('visible');
-    }
-    if (section3.getBoundingClientRect().top <= window.innerHeight) {
-        section3.classList.add('visible');
-    }
-    if (section4.getBoundingClientRect().top <= window.innerHeight) {
-        section4.classList.add('visible');
-    }
-    if (section5.getBoundingClientRect().top <= window.innerHeight) {
-        section5.classList.add('visible');
-    }
+    // if (section1.getBoundingClientRect().top <= window.innerHeight) {
+    //     section1.classList.add('visible');
+    // }
+    // if (section2.getBoundingClientRect().top <= window.innerHeight) {
+    //     section2.classList.add('visible');
+    // }
+    // if (section3.getBoundingClientRect().top <= window.innerHeight) {
+    //     section3.classList.add('visible');
+    // }
+    // if (section4.getBoundingClientRect().top <= window.innerHeight) {
+    //     section4.classList.add('visible');
+    // }
+    // if (section5.getBoundingClientRect().top <= window.innerHeight) {
+    //     section5.classList.add('visible');
+    // }
 });
